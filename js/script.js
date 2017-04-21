2c108499bba51c20a70feb08b692ad42e5f13984
winterwolff <info@dominic-winter.de>
2017-04-21 19:41:20 +0200

    final frontend

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-$(document).ready(function () {
-  $("#black").click(function () {
-      $('head > link').filter(':last').attr('href', 'css/styles.css');
-  });
-  $("#white").click(function () {
-      $('head > link').filter(':last').attr('href', 'css/white.css');
-  });
-  $("#blue").click(function () {
-      $('head > link').filter(':last').attr('href', 'css/blue.css');
-  });
-  $("#red").click(function () {
-      $('head > link').filter(':last').attr('href', 'css/red.css');
-  });
-  $("#green").click(function () {
-      $('head > link').filter(':last').attr('href', 'css/green.css');
-  });
-});
+
